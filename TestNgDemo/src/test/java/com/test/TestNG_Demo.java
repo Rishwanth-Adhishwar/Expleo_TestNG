43e8dbb7bb189795c5ba35bbe5e5f8292ade5158
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-04-16 16:04:45 +0530

    Today_TestNG_update

---
+++ TestNgDemo/src/test/java/com/test/TestNG_Demo.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+
+public class TestNG_Demo {
+	WebDriver driver;
+  @Test
+  public void validation() {
+	  driver.findElement(By.xpath("//a[@id='login2']")).click();
+	  driver.findElement(By.cssSelector("#loginusername")).sendKeys("rishwanth");
+	  driver.findElement(By.cssSelector("#loginpassword")).sendKeys("rishwa123");
+	  driver.findElement(By.cssSelector("button[onclick='logIn()']")).click();
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  ChromeOptions options=new ChromeOptions();
+	  options.addArguments("--start-maximized");
+	  options.addArguments("--headless");
+	  driver=new ChromeDriver(options);
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	  driver.get("https://demoblaze.com/");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
---
+++ TestNgDemo/src/test/java/com/test/TestNgOrder_Run.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class TestNgOrder_Run {
+  @Test
+  public void a() {
+	  System.out.println("Hi Hello");
+  }
+  
+  @Test
+  public void b() {
+	  System.out.println("Hello Hi");
+  }
+  
+  @Test
+  public void c() {
+	  System.out.println("Hello hi Hello");
+  }
+}

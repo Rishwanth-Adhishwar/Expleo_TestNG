cf03357f1b1fe7acb86fd0c92e41260b21e520d3
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-04-22 12:40:40 +0530

    Done Logs Custom way

--- DPDemoTN/src/test/java/com/test/LoginTestDP.java
+++ DPDemoTN/src/test/java/com/test/LoginTestDP.java
@@ -2,10 +2,14 @@ package com.test;
 
 import org.testng.annotations.Test;
 import com.utils.ExcelDP;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
 import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,38 +21,40 @@ import org.testng.annotations.AfterMethod;
 public class LoginTestDP {
 
 	private static final ThreadLocal<WebDriver> driver1 = new ThreadLocal<>();
+	public static Logger log = LogManager.getLogger(LoginTestDP.class);
 
 	@BeforeMethod
 	public void setup() {
 		driver1.set(new ChromeDriver());
+
 	}
 
-	@Test(dataProvider = "vData", dataProviderClass = ExcelDP.class)
+	@Test(priority = 1, dataProvider = "vData", dataProviderClass = ExcelDP.class)
 	public void validLogin(String uname, String upass) {
 
 		WebDriver driver = driver1.get();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-
+		log.info("Browser Opened");
 		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo/");
-
+		log.info("URL Launched and Opened");
 		driver.findElement(By.cssSelector(".fa.fa-user")).click();
 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Login"))).click();
-
+		log.info("Entering username: " + uname);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-email"))).sendKeys(uname);
-
+		log.warn("Remember Your Password");
 		driver.findElement(By.id("input-password")).sendKeys(upass);
 
 		driver.findElement(By.cssSelector("input[value='Login']")).click();
 
-		String actual = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='My Account']"))).getText();
+		String actual = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='My Account']")))
+				.getText();
 
 		Assert.assertEquals(actual, "My Account", "Login Successc");
 	}
 
-
-	@Test(dataProvider = "iData", dataProviderClass = ExcelDP.class)
+	@Test(priority = 2, dataProvider = "iData", dataProviderClass = ExcelDP.class)
 	public void invalidLogin(String uname, String upass) {
 
 		WebDriver driver = driver1.get();
@@ -67,37 +73,52 @@ public class LoginTestDP {
 
 		driver.findElement(By.cssSelector("input[value='Login']")).click();
 
-		String actual = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert-danger"))).getText();
+		String actual = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert-danger")))
+				.getText();
 
-		String expected = "Warning: No match for E-Mail Address and/or Password.";
+		String expected = "Warning: No match for  Address and/or Password.";
 
-		Assert.assertTrue(actual.contains(expected), "Error message not displayed properly");
+		try {
+			Assert.assertTrue(actual.contains(expected));
+			log.info("Login Successful");
+		} catch (AssertionError e) {
+			log.warn("Invalid username:" + uname);
+			log.error(uname + "is invalid" + e.getMessage());
+
+		}
 	}
-	@Parameters({"wordv"}) 
-	@Test 
-	public void searchValid(String word) { 
-		WebDriver driver=driver1.get(); 
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(15)); 
-		driver.manage().window().maximize(); 
-		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home"); 
-		driver.findElement(By.cssSelector("input[placeholder='Search']")).sendKeys(word); 
-		driver.findElement(By.cssSelector("button[class='btn btn-default btn-lg']")).click(); 
-		String actual=wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[id='content'] div:nth-child(1) div:nth-child(1) div:nth-child(2) div:nth-child(1) h4:nth-child(1) a:nth-child(1)"))).getText(); 
-		Assert.assertEquals(actual.contains("mac"),false,"Products Found");
-		} 
-	@Parameters({"wordiv"}) 
-	@Test 
-	public void searchInValid(String word) {
-		WebDriver driver=driver1.get(); 
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(15)); 
-		driver.manage().window().maximize(); 
+
+	@Parameters({ "wordv" })
+	@Test(priority = 3)
+	public void searchValid(String word) {
+		WebDriver driver = driver1.get();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
-		driver.findElement(By.cssSelector("input[placeholder='Search']")).sendKeys(word); 
-		driver.findElement(By.cssSelector("button[class='btn btn-default btn-lg']")).click(); 
-		String actual=wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > p:nth-child(7)"))).getText(); 
-		String expected="There is no product that matches the search criteria."; 
-		Assert.assertEquals(actual, expected,"Products Not Found"); }
-	
+		driver.findElement(By.cssSelector("input[placeholder='Search']")).sendKeys(word);
+		driver.findElement(By.cssSelector("button[class='btn btn-default btn-lg']")).click();
+		String actual = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(
+				"div[id='content'] div:nth-child(1) div:nth-child(1) div:nth-child(2) div:nth-child(1) h4:nth-child(1) a:nth-child(1)")))
+				.getText();
+		Assert.assertEquals(actual.contains("mac"), false, "Products Found");
+	}
+
+	@Parameters({ "wordiv" })
+	@Test(priority = 4)
+	public void searchInValid(String word) {
+		WebDriver driver = driver1.get();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
+		driver.findElement(By.cssSelector("input[placeholder='Search']")).sendKeys(word);
+		driver.findElement(By.cssSelector("button[class='btn btn-default btn-lg']")).click();
+		String actual = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By
+						.cssSelector("body > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > p:nth-child(7)")))
+				.getText();
+		String expected = "There is no product that matches the search criteria.";
+		Assert.assertEquals(actual, expected, "Products Not Found");
+	}
 
 	@AfterMethod
 	public void tearDown() {
--- DPDemoTN/src/test/java/com/utils/ExcelDP.java
+++ DPDemoTN/src/test/java/com/utils/ExcelDP.java
@@ -19,39 +19,28 @@ public class ExcelDP {
     }
 
     public static Object[][] excelDataProvider(String sheetName) throws IOException {
-
         String path = "C:\\Users\\krish\\git\\testng\\DPDemoTN\\src\\test\\resources\\tutorialsDemoTestDatas.xlsx";
-
         return getExcelData(path, sheetName);
     }
 
     private static Object[][] getExcelData(String file, String sheetName) throws IOException {
-
         Object[][] data = null;
-
         FileInputStream fis = new FileInputStream(file);
         XSSFWorkbook work = new XSSFWorkbook(fis);
         XSSFSheet sheet = work.getSheet(sheetName);
-
         int noOfRows = sheet.getPhysicalNumberOfRows();
         int noOfCols = sheet.getRow(0).getLastCellNum();
-
         data = new Object[noOfRows - 1][noOfCols];
-
         DataFormatter formatter = new DataFormatter();
-
         for (int i = 1; i < noOfRows; i++) {
             XSSFRow row = sheet.getRow(i);
-
             for (int j = 0; j < noOfCols; j++) {
                 Cell cell = row.getCell(j);
                 data[i - 1][j] = formatter.formatCellValue(cell);
             }
         }
-
         work.close();
         fis.close();
-
         return data;
     }
 }
